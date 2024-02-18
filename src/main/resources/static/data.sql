8934cec3927008ad172c349e413b5cdfabe5e2e0
jeeveshmishra <jeevesh.mishra@gmail.com>
2024-02-19 00:13:15 +0100

    Added sql scripts to make DB ready for the application functionality to be built upon.

---
+++ src/main/resources/static/data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('001', 'Rolex', 'Rolex watch - James bond 007 collection', 100) ;
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('002', 'Michael Kors', 'Michael Kors watch - 2008 limited edition collection', 80) ;
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('003', 'Swatch', 'Swatch watch - Switzerland collection', 50);
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('004', 'Casio', 'Casio watch - Japan olympics  collection', 30) ;
+
+
+INSERT INTO WATCH_CATALOG_DISCOUNT(item_id, discount_description, discount_item_count, discount_price, is_active) VALUES (1, '3 for 200', 3, 200, true);
+INSERT INTO WATCH_CATALOG_DISCOUNT(item_id, discount_description, discount_item_count, discount_price, is_active) VALUES (2, '2 for 120', 2, 120, true);
---
+++ src/test/resources/static/sql/create_schema_insert_data.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA SHOP;
+
+SET SCHEMA SHOP;
+
+CREATE TABLE IF NOT EXISTS WATCH_CATALOG
+(
+    ITEM_ID SERIAL PRIMARY KEY,
+    WATCH_ID VARCHAR,
+    WATCH_NAME VARCHAR,
+    WATCH_DESCRIPTION VARCHAR,
+    UNIT_PRICE INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS WATCH_CATALOG_DISCOUNT
+(
+    DISCOUNT_ID          SERIAL PRIMARY KEY,
+    ITEM_ID              INT references WATCH_CATALOG (ITEM_ID),
+    DISCOUNT_DESCRIPTION VARCHAR,
+    DISCOUNT_ITEM_COUNT  INTEGER,
+    DISCOUNT_PRICE       INTEGER,
+    IS_ACTIVE            BOOLEAN
+);
+
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('001', 'Rolex', 'Rolex watch - James bond 007 collection', 100) ;
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('002', 'Michael Kors', 'Michael Kors watch - 2008 limited edition collection', 80) ;
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('003', 'Swatch', 'Swatch watch - Switzerland collection', 50);
+INSERT INTO WATCH_CATALOG(watch_id, watch_name, watch_description, unit_price) VALUES('004', 'Casio', 'Casio watch - Japan olympics  collection', 30) ;
+
+
+INSERT INTO WATCH_CATALOG_DISCOUNT(item_id, discount_description, discount_item_count, discount_price, is_active) VALUES (1, '3 for 200', 3, 200, true);
+INSERT INTO WATCH_CATALOG_DISCOUNT(item_id, discount_description, discount_item_count, discount_price, is_active) VALUES (2, '2 for 120', 2, 120, true);
